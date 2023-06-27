a469dcb8d5876b01ec858d6b9e5ee35734321036
Abdul Islam <abdul.islam1120@gmail.com>
2023-06-28 00:04:37 +0500

    check and display variables and data types

---
+++ JavaScript Fundamentals - I/Assignment - 2/script.js
@@ -0,0 +1,11 @@
+//.......................Assignment # 2...........................
+let country = "Pakistan";
+let continent = "Asia";
+let population = 250;
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof continent);
