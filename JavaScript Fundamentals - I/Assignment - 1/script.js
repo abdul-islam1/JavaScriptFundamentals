52f9ed0360194c265bdcc41f6cb1061b9ef629f2
Abdul Islam <abdul.islam1120@gmail.com>
2023-06-27 23:58:47 +0500

    show output and show data types

---
+++ JavaScript Fundamentals - I/Assignment - 1/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// .......................Assignment # 1..........................
+let country = "Pakistan";
+let continent = "Asia";
+let population = "250 million";
+console.log(country);
+console.log(continent);
+console.log(population);
