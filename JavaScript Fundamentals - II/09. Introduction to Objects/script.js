51d16e6dce01934c4335ad337e946f9f6ed0ceb0
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-07-21 23:58:49 +0500

    Introduction to Objects

---
+++ JavaScript Fundamentals - II/09. Introduction to Objects/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const myCountry = {
+  country: "Pakistan",
+  capital: "Islamabad",
+  language: "Urdu",
+  population: 220,
+  neighbours: ["India", "Afghanistan", "Iran", "China", "Iran"],
+};
+
+console.log(myCountry);
