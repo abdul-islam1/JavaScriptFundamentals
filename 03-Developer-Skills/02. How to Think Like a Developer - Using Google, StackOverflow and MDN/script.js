0e989f006981adf955f4c76282d4e1b0fcc567d4
Abdul Islam <130133836+abdul-islam1@users.noreply.github.com>
2023-08-04 23:52:28 +0500

    final second commit Using Google, StackOverflow and MDN - Second Problem

--- 03-Developer-Skills/02. How to Think Like a Developer - Using Google, StackOverflow and MDN/script.js
+++ 03-Developer-Skills/02. How to Think Like a Developer - Using Google, StackOverflow and MDN/script.js
@@ -20,3 +20,26 @@ const calcTempAmplitude = (temps) => {
 const amplitude = calcTempAmplitude(temperatures);
 
 console.log(amplitude);
+
+//////////////////////////////////////////////////
+// PROBLEM 2:
+// Two arrays now!!!
+
+// const calcTempAmplitude = (t1, t2) => {
+//   const temps = t1.concat(t2);
+//   let min = temps[0];
+//   let max = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude([-1, 7, 8], [3, 7, 4]);
+
+// console.log(amplitude);
